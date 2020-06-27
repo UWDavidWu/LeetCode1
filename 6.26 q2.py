0b1b4198426a252d8640a4464ad977d6147e7918
UWDavidWu <wu.dinghang@outlook.com>
2020-06-26 23:31:27 -0400

    Update 6.26 q2.py

--- 6.26 q2.py
+++ 6.26 q2.py
@@ -1,16 +1,15 @@
 '''
-Given an array of integers, return indices 
-of the two numbers such that they add up to a specific target.
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
 
-You may assume that each input would 
-have exactly one solution, and you may not use the same element twice.
+Example 1:
 
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,return [0, 1].
-
+Input: 121
+Output: true
 '''
 
+def palindrome(num):
+    return str(num) == str(num)[::-1]
 
+
+print(palindrome(121) == True)
+print(palindrome(10) == False)
